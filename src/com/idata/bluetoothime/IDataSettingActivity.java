1f1268de25aad867e01938fd8794991efef660b2
lichao3140 <396229938@qq.com>
2018-06-12 11:34:01 +0800

    输入法配置

---
+++ src/com/idata/bluetoothime/IDataSettingActivity.java
@@ -0,0 +1,13 @@
+package com.idata.bluetoothime;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class IDataSettingActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_setting_idata);
+	}
+}
